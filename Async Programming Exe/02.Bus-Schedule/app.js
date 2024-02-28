6c0a27f559a5dbf2955f7e76c3a194503c2524d8
peaceGloriole <dmtrpwlw@icloud.com>
2024-02-28 20:59:54 +0200

    bus schedule done

--- Async Programming Exe/02.Bus-Schedule/app.js
+++ Async Programming Exe/02.Bus-Schedule/app.js
@@ -1,11 +1,31 @@
 function solve() {
+    const departBtn = document.querySelector(`#depart`);
+    const arriveBtn = document.querySelector(`#arrive`);
+    const spanInfo = document.querySelector(`span`);
 
-    function depart() {
-        console.log('Depart TODO...');
+    const departUrl = `http://localhost:3030/jsonstore/bus/schedule/`;
+    const stop = {
+        currentStop: ``,
+        nextStop: `depot`
+    };
+
+    async function depart() {
+        const request = await fetch(departUrl + stop.nextStop);
+        const data = await request.json();
+
+        stop.currentStop = data.name;
+        stop.nextStop = data.next;
+
+        spanInfo.textContent = `Next stop ${stop.currentStop}`;
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        spanInfo.textContent = `Arriving at ${stop.currentStop}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
