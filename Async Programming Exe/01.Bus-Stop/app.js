c90f3e8da758573e298c31a07a69515bd8ba7a57
peaceGloriole <dmtrpwlw@icloud.com>
2024-02-28 20:15:12 +0200

    fixed

--- Async Programming Exe/01.Bus-Stop/app.js
+++ Async Programming Exe/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const stopIdRef = document.getElementById(`stopId`);
+  const stopId = stopIdRef.value;
+  const stopNameRef = document.getElementById(`stopName`);
+  const busesRef = document.getElementById(`buses`);
+
+  const url = 'http://localhost:3030/jsonstore/bus/businfo/' + stopId;
+
+  try {
+    const request = await fetch(url);
+    const data = await request.json();
+
+    busesRef.innerHTML = ``;
+
+    stopNameRef.textContent = data.name;
+    appendChild(Object.entries(data.buses));
+  } catch (error) {
+      stopNameRef.textContent = 'Error';
+      busesRef.innerHTML = ``;
+  }
+
+  function appendChild(data) {
+    for ([bus, time] of data) {
+      let li = document.createElement('li');
+      li.textContent = `Bus ${bus} arrives in ${time} minutes`;
+      busesRef.appendChild(li);
+    }
+  }
 }
