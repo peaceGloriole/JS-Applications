41ec8ec90c4a7ca3735a041e7423779937bc1e37
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-03 18:28:25 +0200

    75/100 phonebook

--- Remote Data and Authentications Exe/02.Phonebook/app.js
+++ Remote Data and Authentications Exe/02.Phonebook/app.js
@@ -1,5 +1,71 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById(`btnLoad`).addEventListener(`click`, loadPhonebook);
+    document.getElementById(`btnCreate`).addEventListener(`click`, createPhonebook);
+    const url = `http://localhost:3030/jsonstore/phonebook`;
+
+    async function loadPhonebook() {
+        const phonebookUl = document.getElementById(`phonebook`);
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            phonebookUl.innerHTML = ``;
+            Object.entries(data).forEach(([id, phonebook]) => {
+                const li = document.createElement(`li`);
+                li.textContent = `${phonebook.person}: ${phonebook.phone}`;
+                const deleteBtn = document.createElement(`button`);
+                deleteBtn.textContent = `Delete`;
+            
+                deleteBtn.addEventListener(`click`, () => deletePhonebook(id));
+                
+                li.appendChild(deleteBtn);
+                phonebookUl.appendChild(li);
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function deletePhonebook(id) {
+        const deleteUrl = `http://localhost:3030/jsonstore/phonebook/${id}`;
+
+        try {
+            const response = await fetch(deleteUrl, {
+                method: `delete`
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+
+        } catch (error) {
+            alert(error.message);
+        }
+
+        loadPhonebook();
+    }
+
+    async function createPhonebook() {
+        let newPersonRef = document.getElementById(`person`);
+        let newPhoneRef = document.getElementById(`phone`);
+
+        const newPerson = newPersonRef.value;
+        const newPhone = newPhoneRef.value;
+
+        const response = await fetch(url, {
+            method: `post`,
+            headers: { 'Content-Type': `application/json` },
+            body: JSON.stringify({ person: newPerson, phone: newPhone })
+        });
+
+        newPersonRef.value = ``;
+        newPhoneRef.value = ``;
+
+        loadPhonebook();
+    }
+
 }
 
 attachEvents();
