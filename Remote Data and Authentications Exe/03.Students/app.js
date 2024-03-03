82070eeee4e2d93f834532157160d5aed4cbf3ac
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-03 20:25:34 +0200

    bullshiticus commits

--- Remote Data and Authentications Exe/03.Students/app.js
+++ Remote Data and Authentications Exe/03.Students/app.js
@@ -1,1 +1,49 @@
-console.log('TODO...');
+function attachEvents() {
+  const url = `http://localhost:3030/jsonstore/collections/students`;
+  document.getElementById(`submit`).addEventListener(`click`, addStudent);
+
+
+  async function addStudent(e) {
+    e.preventDefault();
+
+    const firstNameRef = document.querySelector("input['name=firstName']");
+    const lastNameRef = document.querySelector("input['name=lastName']");
+    const facultyNumberRef = document.querySelector("input['name=facultyNumber']");
+    const gradeRef = document.querySelector("input['name=grade']");
+
+    const firstName = firstNameRef.value;
+    const lastName = lastNameRef.value;
+    const facultyNumber = facultyNumberRef.value;
+    const grade = gradeRef.value;
+
+    if (firstName === "" || lastName === "" || facultyNumber === "" || grade === "") {
+      return;
+    }
+
+    const response = await fetch(url, {
+      method: `post`,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, facultyNumber, grade }),
+    });
+
+    if(!response.ok){
+      const error = await response.json();
+      return alert(error.message);
+    }
+
+    firstNameRef.value = "";
+    lastNameRef.value = "";
+    facultyNumberRef.value = "";
+    gradeRef.value = "";
+
+    loadsStudents();
+  }
+
+  async function loadsStudents() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const students = Object.values(data).map(createStudent).join("");
+    document.querySelector("tbody").innerHTML = students;
+  }
+}
--- Remote Data and Authentications Exe/04.Book-library/app.js
+++ Remote Data and Authentications Exe/04.Book-library/app.js
@@ -1,1 +1,52 @@
-console.log('My requests...')
+const getAllBooksUrl = `http://localhost:3030/jsonstore/collections/books`;
+document.getElementById(`loadBooks`).addEventListener(`click`, loadBooks);
+document.querySelector('form button').addEventListener('click', createBook);
+
+async function loadBooks() {
+  const response = await fetch(getAllBooksUrl);
+
+  try {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+  } catch (error) {
+    alert(error.message);
+  }
+
+}
+
+async function createBook() {
+  const title = document.querySelector("form input[name='title']").value;
+  const author = document.querySelector("form input[name='author']").value;
+
+  if (title === '' || author === '') {
+    return
+  }
+
+  const newBook = {
+    title,
+    author
+  }
+
+  const response = await fetch(getAllBooksUrl, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newBook)
+  });
+
+  try {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+  } catch (error) {
+    alert(error.message);
+  }
+
+  document.querySelector("form input[name='title']").value = '';
+  document.querySelector("form input[name='author']").value = '';
+  loadBooks();
+}
