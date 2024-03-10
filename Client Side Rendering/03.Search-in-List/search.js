541458f75b5827b02cd632f3f9aa5b182c26f388
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-10 17:07:50 +0200

    fixed

--- Client Side Rendering/03.Search-in-List/search.js
+++ Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,26 @@
-function search() {
-   // TODO
-}
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const divId = document.getElementById('towns');
+const input = document.getElementById('searchText');
+const resultDiv = document.getElementById('result');
+
+const townTemplate = (town) => html`
+  <li>${town}</li>
+`;
+
+const renderTowns = (towns) => {
+  const townList = html`<ul>${towns.map(town => townTemplate(town))}</ul>`;
+  render(townList, divId);
+};
+
+renderTowns(towns);
+
+document.querySelector('button').addEventListener('click', () => {
+  const searchText = input.value.toLowerCase();
+  const filteredTowns = towns.filter(town => town.toLowerCase().includes(searchText));
+  renderTowns(filteredTowns);
+
+  // Display the search result count
+  resultDiv.textContent = `${filteredTowns.length} result(s) found`;
+});
