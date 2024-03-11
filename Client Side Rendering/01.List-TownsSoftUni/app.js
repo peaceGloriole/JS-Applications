f0502c46c6e41002f6c4fb928c440ded924c52bd
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-11 18:42:17 +0200

    soft

--- Client Side Rendering/01.List-TownsSoftUni/app.js
+++ Client Side Rendering/01.List-TownsSoftUni/app.js
@@ -1,9 +1,24 @@
+import { html, render } from '/node_modules/lit-html/lit-html.js';
+
 const form = document.querySelector(`form`).addEventListener(`submit`, onSubmit);
+const root = document.getElementById(`root`);
 
 function onSubmit(e) {
   e.preventDefault();
 
   const formData = new FormData(e.target);
   const townsList = formData.get(`towns`).split(`, `);
+  renderer(createTemplate(townsList));
+}
 
+function createTemplate(data) {
+  return html`
+  <ul>
+    ${data.map(t => html`<li>${t}</li>`)}
+  </ul>
+  `;
+}
+
+function renderer(template) {
+  render(template, root);
 }
