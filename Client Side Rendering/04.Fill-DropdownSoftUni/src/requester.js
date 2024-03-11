19f9fa77282614c368655a841fde522c55b4511a
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-11 21:27:41 +0200

    to check my file and update. map instead of forEach and values instead of ..

---
+++ Client Side Rendering/04.Fill-DropdownSoftUni/src/requester.js
@@ -0,0 +1,31 @@
+async function requester(method, url, data) {
+  const option = {
+    method
+  }
+
+  if (data) {
+    option[`headers`] = { 'Content-Type': 'application/json' };
+    option[`body`] = JSON.stringify(data);
+  }
+
+  try {
+    const response = await fetch(url, option);
+    const resData = await response.json();
+    return resData;
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+async function get(url) {
+  return requester(`GET`, url);
+}
+
+async function post(url, data) {
+  return requester(`POST`, url, data);
+}
+
+export const dataApi = {
+  get,
+  post
+}
