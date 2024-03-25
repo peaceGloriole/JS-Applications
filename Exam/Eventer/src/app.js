bbc280e04ca8bfcc059ea3990bac7ee2672c6f6a
peaceGloriole <dmtrpwlw@icloud.com>
2024-03-25 21:45:39 +0200

    wrong localhost or path ???

--- Exam/Eventer/src/app.js
+++ Exam/Eventer/src/app.js
@@ -3,6 +3,6 @@ import { catalogPage } from './views/catalog.js';
 import { homePage } from './views/home.js';
 
 page(`/`, homePage());
-page(`/catalog`, catalogPage); 
+page(`/catalog`, catalogPage()); 
 
 page.start();
